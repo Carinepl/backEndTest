7a80cb13e65673444be18b0e412964655f36efbe
carine.lima <caricalima1@gmail.com>
2024-11-15 11:59:45 -0300

    chore: separada a lógica dos controlles

--- src/userCase/deleteUser.js
+++ src/userCase/deleteUser.js
@@ -1,7 +1,19 @@
-const users = require('../repositores/users');
-const { User } = require('../models/user');
+const users = require('../repositories/users'); 
+const {User} = require('../models/user');
 
-const deleteUser = (data) => {
+const deleteUser = (id) => {
+    const userIndex = users.findIndex((_user) => _user.id === parseInt(id));
+
+    if (userIndex === -1) {
+        throw new Error("Recurso não encontrado");
+    }
+
+    users.splice(userIndex, 1);
+};
+
+module.exports = { deleteUser };
+
+
+    
     
 
-}
